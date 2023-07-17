4dc590553cb3dc51ef820ebaeb2444891c4d0959
Ryu4949 <kyong494949@gmail.com>
2023-07-17 15:52:50 +0900

    [Mod] set basic game-board

--- React/tictactoe/src/App.js
+++ React/tictactoe/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Board from "./components/Board";
+import "./App.css";
 
 function App() {
   return (
     <div className="game">
       <div className="game-board">
-        game-board
+        <Board></Board>
       </div>
       <div className="game-info">
         game-info
