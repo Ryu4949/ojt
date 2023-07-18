89a9b1cceb8c16a513d986215fb4e341461fb42e
Ryu4949 <kyong494949@gmail.com>
2023-07-18 13:33:49 +0900

    [Add] add part02_ch15

---
+++ Java/Java/src/Part_02_OOP/ch15/Taxi.java
@@ -0,0 +1,12 @@
+package Part_02_OOP.ch15;
+
+public class Taxi {
+
+    String taxiName;
+    int taxiIncome;
+
+    public Taxi(String taxiName) {
+        this.taxiName = taxiName;
+        this.taxiIncome = 0;
+    }
+}
