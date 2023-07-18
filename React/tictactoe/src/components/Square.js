c78c839345dacbe3450c2db45a37342eb6433ae5
Ryu4949 <kyong494949@gmail.com>
2023-07-18 15:34:07 +0900

    [Mod] change to functional components

--- React/tictactoe/src/components/Square.js
+++ React/tictactoe/src/components/Square.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Square.css";
 
-export default class Square extends React.Component {
-    
-    render() {
-        return (
-            <button className="square" onClick={() => {this.props.onClick() }}>
-                {this.props.value}
-            </button>
-        )
-    }
+// export default class Square extends React.Component {
+const Square = ({onClick, value}) => {
+
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    )
 }
+export default Square
