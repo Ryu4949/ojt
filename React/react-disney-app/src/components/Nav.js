733fdf34f49771e5280984ce4b5e05b86a45a19c
Ryu4949 <kyong494949@gmail.com>
2023-07-24 16:43:02 +0900

    [Mod] set event on navbar

--- React/react-disney-app/src/components/Nav.js
+++ React/react-disney-app/src/components/Nav.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    });
+    return () => {
+        window.removeEventListener("scroll", () => {})
+    }
+  })
+
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
         <Logo>
             <img
             alt="Disney Plus Logo"
@@ -22,7 +37,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${props => props.show ? "#090b13" : "transparent"};
   display: flex;
   justify-content: space-between;
   align-items: center;
