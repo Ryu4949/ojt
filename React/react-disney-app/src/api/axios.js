40411f9137df50dc729544fec1edb5aafb203546
Ryu4949 <kyong494949@gmail.com>
2023-07-25 16:10:00 +0900

    [Mod] get movie data on banner

--- React/react-disney-app/src/api/axios.js
+++ React/react-disney-app/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import secret from "../../secrets.json";
+import secret from "../secrets.json";
 
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
--- React/react-disney-app/src/components/Banner.js
+++ React/react-disney-app/src/components/Banner.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import axios from '../api/axios'
+import React, { useEffect, useState } from 'react'
+import requests from '../api/request'
 
 const Banner = () => {
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+
+  }, [])
+
+  const fetchData = async() => {
+    const response = await axios.get(requests.fetchNowPlaying)
+    console.log(response);
+
+    const movieId = response.data.results[
+      Math.floor(Math.random() * response.data.results.length)
+    ].id
+
+    // 특정 영화의 더 상세한 정보 가져오기
+    const {data: movieDetail}  = await axios.get(`movie/${movieId}`,
+    {params: {append_to_response: "videos"},});
+    setMovie(movieDetail);
+  }
+  
+
   return (
     <div>
-      
+      Banner
     </div>
   )
 }
