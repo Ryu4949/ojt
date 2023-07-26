bf6431f582af238ed6a0d09f4767f7a4153c2b6f
Ryu4949 <kyong494949@gmail.com>
2023-07-26 15:36:48 +0900

    [Fix] fix errors

--- React/react-disney-app/src/components/Banner.js
+++ React/react-disney-app/src/components/Banner.js
@@ -7,12 +7,12 @@ const Banner = () => {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-
+    fetchData();
   }, [])
 
   const fetchData = async() => {
     const response = await axios.get(requests.fetchNowPlaying)
-    console.log(response);
+    console.log(response);  
 
     const movieId = response.data.results[
       Math.floor(Math.random() * response.data.results.length)
@@ -26,9 +26,34 @@ const Banner = () => {
   
 
   return (
-    <div>
-      Banner
-    </div>
+    <header
+    className='banner'
+    style={{
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+      backgroundPosition: "top center",
+      backgroundSize: "cover",
+    }}>
+      <div className='banner__contents'>
+        <h1 className='banner__title'>
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className='banner__buttons'>
+          {movie?.videos?.results[0]?.key &&
+          <button
+          className='banner__button play'
+        >
+          Play
+          </button>
+          }
+        </div>
+        <p className='banner__description'>
+          {movie.overview}
+        </p>
+      </div>
+      <div className='banner__fadeBottom'></div>
+
+    </header>
   )
 }
 
