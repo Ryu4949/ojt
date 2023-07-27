deb80c67de991390ac6a52ea7d5a09e262cb8c64
Ryu4949 <kyong494949@gmail.com>
2023-07-27 15:17:33 +0900

    [Add] add part02_03_ch14

---
+++ Java/Java/src/Part_02_03_OOP_Core/ch14/Calculator.java
@@ -0,0 +1,20 @@
+package Part_02_03_OOP_Core.ch14;
+
+import Part_02_03_OOP_Core.ch11.Calc;
+
+public abstract class Calculator implements Calc {
+
+    @Override
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int substract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+//    public void description() {
+//        System.out.println("더하기와 빼기 기능을 갖춘 계산기입니다.");
+//    }
+}
