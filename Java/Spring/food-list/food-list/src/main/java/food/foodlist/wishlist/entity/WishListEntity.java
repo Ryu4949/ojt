662d68324f5817d6223de35ba713fc0768f601b3
Ryu4949 <kyong494949@gmail.com>
2023-08-01 13:17:56 +0900

    [Add] add wishlist entity

---
+++ Java/Spring/food-list/food-list/src/main/java/food/foodlist/wishlist/entity/WishListEntity.java
@@ -0,0 +1,18 @@
+package food.foodlist.wishlist.entity;
+
+import food.foodlist.db.MemoryDbEntity;
+
+import java.time.LocalDateTime;
+
+public class WishListEntity extends MemoryDbEntity {
+
+    private String title;                   // 장소명
+    private String category;                // 카테고리
+    private String address;                 // 주소
+    private String roadAddress;             // 도로명 주소
+    private String homepageLink;            // 홈페이지 주소
+    private String imageLink;               // 음식, 가게 이미지 주소
+    private boolean isVisit;                // 방문 여부
+    private int visitCount;                 // 방문 카운트
+    private LocalDateTime lastVisitDate;    // 마지막 방문 일자
+}
