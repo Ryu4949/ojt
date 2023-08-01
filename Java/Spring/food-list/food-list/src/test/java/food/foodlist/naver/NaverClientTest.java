67381d509dad6e3f7fec7fdd7f7fcfc21fbcf016
Ryu4949 <kyong494949@gmail.com>
2023-08-01 17:45:53 +0900

    [Add] add search using naver api & naver api test

---
+++ Java/Spring/food-list/food-list/src/test/java/food/foodlist/naver/NaverClientTest.java
@@ -0,0 +1,24 @@
+package food.foodlist.naver;
+
+import food.foodlist.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NaverClientTest {
+
+    @Autowired
+    private NaverClient naverClient;
+
+    @Test
+    public void searchLocalTest() {
+
+        var search = new SearchLocalReq();
+        search.setQuery("갈비집");
+
+        var result = naverClient.searchLocal(search);
+        System.out.println(result);
+    }
+
+}
