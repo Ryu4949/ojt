a0d4f183a73fb023be90a76b4b2e5ff38355e391
Ryu4949 <kyong494949@gmail.com>
2023-08-03 00:08:29 +0900

    [Add] movie detail page

--- React/react-disney-app/src/pages/DetailPage/index.js
+++ React/react-disney-app/src/pages/DetailPage/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "../../api/axios";
 
 const DetailPage = () => {
+  let { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        `/movie/${movieId}`
+      )
+      setMovie(response.data);
+    }
+    fetchData();
+  }, [movieId])
+
   return (
     <div>
-      
+      Detail Page
     </div>
   )
 }
