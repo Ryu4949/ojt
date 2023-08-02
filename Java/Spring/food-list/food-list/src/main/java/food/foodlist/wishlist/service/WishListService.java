0167bf69aec07560bb3a9b3d3c21f8375b17ff01
Ryu4949 <kyong494949@gmail.com>
2023-08-02 10:13:06 +0900

    [Add] add wishlist service

---
+++ Java/Spring/food-list/food-list/src/main/java/food/foodlist/wishlist/service/WishListService.java
@@ -0,0 +1,38 @@
+package food.foodlist.wishlist.service;
+
+import food.foodlist.naver.NaverClient;
+import food.foodlist.naver.dto.SearchImageReq;
+import food.foodlist.naver.dto.SearchLocalReq;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WishListService {
+
+    private final NaverClient naverClient;
+
+    public void search(String query) {
+
+        //지역검색
+        var searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
+
+        var searchLocalRes = naverClient.searchLocal(searchLocalReq);
+
+        if(searchLocalRes.getTotal() > 0) {
+            var item = searchLocalRes.getItems().stream().findFirst().get();
+
+            var imageQuery = item.getTitle().replaceAll("<[^>]*>", "");
+            var searchImageReq = new SearchImageReq();
+            searchImageReq.setQuery(imageQuery);
+
+            var searchImageRes = naverClient.searchImage(searchImageReq);
+
+            if(searchImageRes.getTotal() > 0) {
+                // 결과물 리턴
+                
+            }
+        }
+    }
+}
