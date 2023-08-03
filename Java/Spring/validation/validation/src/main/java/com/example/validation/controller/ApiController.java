a28b64159dbd20070c7544e076c471d8b52ac21b
Ryu4949 <kyong494949@gmail.com>
2023-08-03 17:02:28 +0900

    [Add] add validation

--- Java/Spring/validation/validation/src/main/java/com/example/validation/controller/ApiController.java
+++ Java/Spring/validation/validation/src/main/java/com/example/validation/controller/ApiController.java
@@ -1,8 +1,11 @@
 package com.example.validation.controller;
 
 import com.example.validation.dto.User;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @PostMapping("/user")
-    public ResponseEntity user(@RequestBody User user) {
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()){
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("field : "+field.getField());
+                System.out.println(message);
+
+                sb.append("field : "+field.getField());
+                sb.append("message : "+message);
+            });
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+
+
         System.out.println(user);
 
         // 검증을 거쳐야할 데이터가 많아지면 점점 코드가 복잡해짐
