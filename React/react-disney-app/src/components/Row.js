7eaa184a94a4ec8b713d6b4d4e62cae1d7259cce
Ryu4949 <kyong494949@gmail.com>
2023-08-04 00:29:04 +0900

    [Chore] downgrade swiper

--- React/react-disney-app/src/components/Row.js
+++ React/react-disney-app/src/components/Row.js
@@ -35,25 +35,28 @@ const Row = ({title, id, fetchUrl}) => {
   return (
     <div>
       <h2>{title}</h2>
-      <Swiper 
+      <Swiper
+        // install Swiper modules
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        loop={true} // loop기능 사ㅇㅕ부
-        navigation  // arrow 버튼 사용 여부
-        pagination={{ clickable: true}}
+        loop={true} //loop 기능을 사용할 것인지
+        navigation={true} // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} //
         >
           <Content id={id}>
-      {movies.map(movie => (
-        <SwiperSlide>
-            <img
-            key={movie.id}
-            className="row__poster"
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            alt={movie.name}
-            onClick={() => handleClick(movie)} />
-            </SwiperSlide>
-          ))}
+            {movies.map(movie => (
+              <SwiperSlide>
+                <Wrap>
+                  <img
+                  key={movie.id}
+                  className="row__poster"
+                  src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)} />
+                </Wrap>
+              </SwiperSlide>
+                ))}
           </Content>
-          </Swiper>
+        </Swiper>
 
       {modalOpen &&
       <MovieModal
@@ -73,4 +76,34 @@ const Container = styled.div`
 
 const Content = styled.div``;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+              rgb(0 0 0/73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index:1;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
