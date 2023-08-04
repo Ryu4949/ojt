5e5c8089a9669b55cbb9ffb46c241a2811ceeea6
Ryu4949 <kyong494949@gmail.com>
2023-08-04 14:32:13 +0900

    [Add] add controller advice, error & error response dto

--- Java/Spring/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ Java/Spring/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -1,16 +1,27 @@
-package main.java.com.example.exception.controller;
+package com.example.exception.controller;
 
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import main.java.com.example.exception.dto.User;
+import com.example.exception.dto.User;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
--- Java/Spring/exception/src/main/java/com/example/exception/dto/User.java
+++ Java/Spring/exception/src/main/java/com/example/exception/dto/User.java
@@ -1,4 +1,4 @@
-package main.java.com.example.exception.dto;
+package com.example.exception.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
