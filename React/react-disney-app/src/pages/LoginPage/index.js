7abfdd9c0c72923a0a1347a183d8155244194249
Ryu4949 <kyong494949@gmail.com>
2023-08-06 01:58:50 +0900

    [Add] add login page

--- React/react-disney-app/src/pages/LoginPage/index.js
+++ React/react-disney-app/src/pages/LoginPage/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { styled } from 'styled-components'
 
 const LoginPage = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Content>
+        <Center>
+          <LogoOne src="/images/cta-logo-one.svg"></LogoOne>
+          <SignUpLink>지금 가ㅂ</SignUpLink>
+          <Description>
+            영화에 대한 프리미어 액세스를 얻으십시오.
+            디즈니 플러스 가격은 다음주부터 1000원 인상입니다.
+          </Description>
+          <LogoTwo src="/images/cta-logo-two.png" />
+        </Center>
+      </Content>
+    </Container>
   )
 }
 
 export default LoginPage
+
+const Container = styled.section``;
+
+const Content = styled.div``;
+
+const Center = styled.div``;
+
+const LogoOne = styled.img``;
+
+const SignUpLink = styled.a``;
+
+const Description = styled.p``;
+
+const LogoTwo = styled.img``;
+
