ca508eb2a4138fe407d5986304d2f020addcfe54
Ryu4949 <kyong494949@gmail.com>
2023-08-06 23:47:17 +0900

    [Style] style drop down button

--- React/react-disney-app/src/components/Nav.js
+++ React/react-disney-app/src/components/Nav.js
@@ -95,10 +95,36 @@ const Nav = () => {
 
 export default Nav
 
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100%;
+  opacity: 0;
+`;
+
 const SignOut = styled.div`
   position: relative;
   height: 48px;
   width: 48px;
+  display: flex;
+  cursor: pointer;
+  aligh-items: center;
+  justify-content: center;
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 const UserImg = styled.img`
@@ -106,8 +132,6 @@ const UserImg = styled.img`
   width: 100%;
   height: 100%;
 `;
-
-const DropDown = styled.div``;
 
 const Login = styled.a`
   background-color: rgba(0,0,0,0.6);
