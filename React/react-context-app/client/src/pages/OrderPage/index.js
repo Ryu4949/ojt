6a0f92a2b496180235094de620576e47a34568df
Ryu4949 <kyong494949@gmail.com>
2023-08-08 15:15:07 +0900

    [Add] add sub types to OrderPage

--- React/react-context-app/client/src/pages/OrderPage/index.js
+++ React/react-context-app/client/src/pages/OrderPage/index.js
@@ -8,6 +8,15 @@ const OrderPage = () => {
         <div>
             <Type orderType="products"></Type>
         </div>
+        <div style={{ display: 'flex', marginTop: 20 }}>
+            <div style={{ width: '50%' }}>
+                <Type orderType="options" />
+            </div>
+            <div style={{ width: '50%' }}>
+                <h2>Total Price: </h2>
+                <button>주문</button>
+            </div>
+        </div>
     </div>
   )
 }
