1baf1fc94f22f0eeae03ebd21bcd31feec697447
Ryu4949 <kyong494949@gmail.com>
2023-08-08 15:02:46 +0900

    [Add] add order page to appjs

--- React/react-context-app/client/src/App.js
+++ React/react-context-app/client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import OrderPage from './pages/OrderPage';
 import SummaryPage from './pages/SummaryPage';
 
 function App() {
   return (
     <div className="App">
+      <OrderPage />
       <SummaryPage />
     </div>
   );
