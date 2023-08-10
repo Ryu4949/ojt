6b47e6f21c5238c057213a6df35a61071d79e428
Ryu4949 <kyong494949@gmail.com>
2023-08-10 14:03:03 +0900

    [Add] install react-redux, add provider

--- React/my-app/src/index.tsx
+++ React/my-app/src/index.tsx
@@ -22,10 +22,12 @@ console.log('store.getState', store.getState());
 
 const render = () => root.render(
   <React.StrictMode>
-    <App 
-    value={store.getState()}
-    onIncrement={() => store.dispatch({type: "INCREMENT"})}
-    onDecrement={() => store.dispatch({type: "DECREMENT"})} />
+    <Provider store={store}>
+      <App 
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: "INCREMENT"})}
+      onDecrement={() => store.dispatch({type: "DECREMENT"})} />
+    </Provider>
   </React.StrictMode>
 );
 render();
