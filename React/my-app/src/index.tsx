bf3852012e45eb5aa6d7293aa5b2e5ca25c3eac8
Ryu4949 <kyong494949@gmail.com>
2023-08-10 15:09:03 +0900

    [Feat] add input box and todo list

--- React/my-app/src/index.tsx
+++ React/my-app/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import counter from './reducers';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
