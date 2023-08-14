4c9605150ee173aa274eaba25dd5ac5b0bc4d3d7
Ryu4949 <kyong494949@gmail.com>
2023-08-14 16:04:31 +0900

    [Add] add user controller(signup)

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserController.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserController.java
@@ -1,4 +1,30 @@
 package security.spring_security_practice.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import security.spring_security_practice.user.dto.UserRegisterDto;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/signup")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String signup() {
+        return "signup";
+    }
+
+    @PostMapping
+    public String signup(
+            @ModelAttribute UserRegisterDto userRegisterDto
+            ) {
+        userService.signup(userRegisterDto.getUsername(), userRegisterDto.getPassword());
+        return "redirect:login";
+    }
 }
