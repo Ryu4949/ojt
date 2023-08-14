76845676e4cd443af2169ea91348359a22f5a911
Ryu4949 <kyong494949@gmail.com>
2023-08-14 15:17:26 +0900

    [Add] add user register dto

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/dto/UserRegisterDto.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/dto/UserRegisterDto.java
@@ -1,4 +1,13 @@
 package security.spring_security_practice.user.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class UserRegisterDto {
+
+    private String username;
+    private String password;
 }
