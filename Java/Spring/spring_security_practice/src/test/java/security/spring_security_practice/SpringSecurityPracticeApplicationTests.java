577b1c39580d033efdf614ba8cf32e486af22a1d
Ryu4949 <kyong494949@gmail.com>
2023-08-14 10:03:22 +0900

    [Add] initial commit for spring security practice

---
+++ Java/Spring/spring_security_practice/src/test/java/security/spring_security_practice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package security.spring_security_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
