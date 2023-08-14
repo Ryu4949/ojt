c2308bb2c87c382c8a42b9757b73b6144f430554
Ryu4949 <kyong494949@gmail.com>
2023-08-14 16:36:26 +0900

    [Add] add password encoder config

---
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/config/PasswordEncoderConfig.java
@@ -0,0 +1,17 @@
+package security.spring_security_practice.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * PasswordEncoder Config
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
