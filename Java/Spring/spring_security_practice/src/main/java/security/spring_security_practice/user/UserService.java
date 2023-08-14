4e2df3a3ca5472ffe1a5ca341a8cc1f6be3279e5
Ryu4949 <kyong494949@gmail.com>
2023-08-14 16:26:56 +0900

    [Add] add user servcie

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserService.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserService.java
@@ -1,4 +1,29 @@
 package security.spring_security_practice.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import security.spring_security_practice.user.entity.User;
+import security.spring_security_practice.user.exception.AlreadyRegisteredUserException;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User signup(
+            String username,
+            String password
+    ) {
+        if (userRepository.findByUserName(username) != null) {
+            throw new AlreadyRegisteredUserException();
+        }
+        return userRepository.save(new User(username, passwordEncoder.encode(password), "ROLE_USER"));
+    }
+
+    public User findByUserName(String username) {
+        return userRepository.findByUserName(username);
+    }
 }
