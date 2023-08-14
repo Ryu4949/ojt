4ba1595623605e2cf3f78caf291a88d11efcf1cd
Ryu4949 <kyong494949@gmail.com>
2023-08-14 16:07:08 +0900

    [Add] add user repository

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserRepository.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserRepository.java
@@ -1,4 +1,9 @@
 package security.spring_security_practice.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import security.spring_security_practice.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserName(String name);
 }
