41a68ddedb57320503efb55d7db99f9c4a9b6448
Ryu4949 <kyong494949@gmail.com>
2023-08-14 11:17:07 +0900

    [Add] add user exceptions

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/exception/AlreadyRegisteredUserException.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/exception/AlreadyRegisteredUserException.java
@@ -1,4 +1,12 @@
 package security.spring_security_practice.user.exception;
 
-public class AlreadyRegisteredUserException {
+public class AlreadyRegisteredUserException extends RuntimeException {
+
+    public AlreadyRegisteredUserException(String message) {
+        super(message);
+    }
+
+    public AlreadyRegisteredUserException() {
+        super("이미 등록된 사용자입니다.");
+    }
 }
--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/exception/UserNotFoundException.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/exception/UserNotFoundException.java
@@ -1,4 +1,12 @@
 package security.spring_security_practice.user.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+        super("등록되지 않은 사용자입니다.");
+    }
 }
