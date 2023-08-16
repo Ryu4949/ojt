d40bf102fe8173aa04ee4e4efe80f8a4aecd3524
Ryu4949 <kyong494949@gmail.com>
2023-08-16 11:23:02 +0900

    [Add] add persistor

--- React/react-disney-app/src/store/index.js
+++ React/react-disney-app/src/store/index.js
@@ -1,8 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { useReducer } from "react";
+import { configureStore } from "@./userSlice";
+import userReducer from './userSlice';
+import { combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+        user: userReducer
+})
+
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: useReducer
-    }
+    reducer: persistedReducer,
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false}),
 })
+
+export const persistor = persistStore(store);
