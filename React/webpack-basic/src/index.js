ebea7dbee6ed6d8b1d48d331a9c7bcbec27d38c4
Ryu4949 <kyong494949@gmail.com>
2023-08-16 13:42:01 +0900

    [Add] import nanoid

--- React/webpack-basic/src/index.js
+++ React/webpack-basic/src/index.js
@@ -1,5 +1,9 @@
 import getRandomAddress from "./randomAddress"
+import { nanoid } from "nanoid";
+
 
 // webpack 설치 없이 실행하면 오류 발생
 // cannot use import statement outside a module
 console.log(getRandomAddress());
+
+console.log(nanoid());
