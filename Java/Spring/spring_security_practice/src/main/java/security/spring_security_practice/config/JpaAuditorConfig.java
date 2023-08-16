335f6b500a9df6703a878078929ba29a1c8a54d9
Ryu4949 <kyong494949@gmail.com>
2023-08-16 17:45:26 +0900

    [Add] add jpa auditor config

---
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/config/JpaAuditorConfig.java
@@ -0,0 +1,9 @@
+package security.spring_security_practice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditorConfig {
+}
