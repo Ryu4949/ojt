161be726cafb240c4030efbfb238383129d4b202
Ryu4949 <kyong494949@gmail.com>
2023-08-16 17:47:38 +0900

    [Add] Initialize Default Config and admin signup in userservice

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserService.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/user/UserService.java
@@ -23,6 +23,16 @@ public class UserService {
         return userRepository.save(new User(username, passwordEncoder.encode(password), "ROLE_USER"));
     }
 
+    public User signupAdmin(
+            String username,
+            String password
+    ) {
+        if (userRepository.findByUsername(username) != null) {
+            throw new AlreadyRegisteredUserException();
+        }
+        return userRepository.save(new User(username, passwordEncoder.encode(password), "ROLE_ADMIN"));
+    }
+
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
