3a940acb8251d0ae5a876c84c647da168c8a66c6
Ryu4949 <kyong494949@gmail.com>
2023-08-16 13:03:59 +0900

    [Add] add middleware and ignored actions

--- React/react-disney-app/src/store/index.js
+++ React/react-disney-app/src/store/index.js
@@ -2,6 +2,7 @@ import userReducer from './userSlice';
 import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 
 const rootReducer = combineReducers({
         user: userReducer
@@ -16,7 +17,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false}),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: {
+    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+    }),
 })
 
 export const persistor = persistStore(store);
