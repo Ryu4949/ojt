f368f45870de03da3c9f8bb055b337916394d50b
Ryu4949 <kyong494949@gmail.com>
2023-08-18 15:09:19 +0900

    [Add] add resource asset

--- React/webpack-basic/src/index.js
+++ React/webpack-basic/src/index.js
@@ -1,6 +1,7 @@
 import getRandomAddress from "./randomAddress"
 import { nanoid } from "nanoid";
 import './styles/main.scss';
+import mainImage from './assets/image.jpeg';
 
 
 // webpack 설치 없이 실행하면 오류 발생
@@ -8,3 +9,6 @@ import './styles/main.scss';
 console.log(getRandomAddress());
 
 console.log(nanoid());
+
+const img = document.getElementById('mainImage');
+img.src = mainImage;
