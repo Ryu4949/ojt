4578bc341b0da103723e2b56bc157381547a360c
Ryu4949 <kyong494949@gmail.com>
2023-08-22 10:18:03 +0900

    [Add] add jwt properties

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/jwt/JwtProperties.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/jwt/JwtProperties.java
@@ -1,4 +1,6 @@
 package security.spring_security_practice.jwt;
 
 public class JwtProperties {
+    public static final int EXPIRATION_TIME = 600000;
+    public static final String COOKIE_NAME = "JWT-AUTHENTICATION";
 }
