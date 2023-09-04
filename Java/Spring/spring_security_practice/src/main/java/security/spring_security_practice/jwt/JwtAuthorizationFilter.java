137a139d70d2b9ce840230bf54bff375299d5f72
Ryu4949 <kyong494949@gmail.com>
2023-09-04 09:30:35 +0900

    [Add] add jwt authorization filter

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/jwt/JwtAuthorizationFilter.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/jwt/JwtAuthorizationFilter.java
@@ -1,4 +1,70 @@
 package security.spring_security_practice.jwt;
 
-public class JwtAuthorizationFilter {
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import security.spring_security_practice.user.UserRepository;
+import security.spring_security_practice.user.entity.User;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+
+    public JwtAuthorizationFilter(
+            UserRepository userRepository
+    ) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+        String token = null;
+        try {
+            token = Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals(JwtProperties.COOKIE_NAME)).findFirst()
+                    .map(Cookie::getValue)
+                    .orElse(null);
+        } catch (Exception ignored) {
+        }
+        if (token != null) {
+            try {
+                Authentication authentication = getUsernamePasswordAuthenticationToken(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                Cookie cookie = new Cookie(JwtProperties.COOKIE_NAME, null);
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+
+    private Authentication getUsernamePasswordAuthenticationToken(String token) {
+        String userName = JwtUtils.getUsername(token);
+        if (userName != null) {
+            User user = userRepository.findByUsername(userName);
+            return new UsernamePasswordAuthenticationToken(
+                    user,
+                    null,
+                    user.getAuthorities()
+            );
+        }
+        return null;
+    }
 }
