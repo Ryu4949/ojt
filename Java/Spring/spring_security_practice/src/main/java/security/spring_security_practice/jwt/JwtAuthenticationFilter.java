feb26c3d26cf101960375676a73afeff5d17c4e0
Ryu4949 <kyong494949@gmail.com>
2023-09-04 09:25:35 +0900

    [Add] add jwt authentication filter

--- Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/jwt/JwtAuthenticationFilter.java
+++ Java/Spring/spring_security_practice/src/main/java/security/spring_security_practice/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,66 @@
 package security.spring_security_practice.jwt;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import security.spring_security_practice.user.entity.User;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                request.getParameter("username"),
+                request.getParameter("password"),
+                new ArrayList<>()
+        );
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult
+    ) throws IOException {
+        User user = (User) authResult.getPrincipal();
+        String token = JwtUtils.createToken(user);
+        // 쿠키 생성
+        Cookie cookie = new Cookie(JwtProperties.COOKIE_NAME, token);
+        cookie.setMaxAge(JwtProperties.EXPIRATION_TIME); // 쿠키의 만료시간 설정
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        response.sendRedirect("/");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException failed
+    ) throws IOException {
+        response.sendRedirect("/login");
+    }
 }
